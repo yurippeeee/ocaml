410b3553a7240d0c91be51f178aa7bd10b9c2300
yurippeeee <isshi0929@icloud.com>
2021-03-14 00:18:51 +0900

    chapter7 no tochu

--- chap5/yugu_jikyu.ml
+++ chap5/yugu_jikyu.ml
@@ -13,4 +13,4 @@ let kyuyo x = kihonkyu + x * (if x < 30 then jikyu else yugu_jikyu)
 
 let test1 = (kyuyo 25 = 23850)
 let test2 = (kyuyo 28 = 26700)
-let test3 = (kyuyo 32 = 30480)
+let test3 = (kyuyo 31 = 30480)
---
+++ chap7/goukei_to_heikin.ml
@@ -0,0 +1,18 @@
+let kamoku_suu = 5.
+
+(* 目的：国語・数学・英語・理科・社会の点数を受取り、合計点と平均点の組を返す *)
+(* goukei : int -> int -> int -> int -> int -> int *)
+let goukei n_lang math eng sci soci = n_lang + math + eng + sci + soci
+
+let test1 = goukei 20 30 40 50 60 = 200
+let test2 = goukei 15 15 15 15 15 = 75
+
+(* 目的：国語・数学・英語・理科・社会の点数を受取り、合計点と平均点の組を返す *)
+(* goukei_to_heikin : int -> int -> int -> int -> int -> (int, float) *)
+let goukei_to_heikin n_lang math eng sci soci 
+    = (goukei n_lang math eng sci soci
+        , (float_of_int (goukei n_lang math eng sci soci)) /. kamoku_suu)
+
+let test3 = goukei_to_heikin 20 30 40 50 60 = (200, 40.)
+let test4 = goukei_to_heikin 15 15 15 15 15 = (75, 15.)
+let test5 = goukei_to_heikin 23 24 22 23 22 = (114, 22.8)
